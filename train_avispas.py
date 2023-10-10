19434f572e4ab2603c76d211c81392c3133ff302
Marian Basti <31198560+marianbasti@users.noreply.github.com>
2023-10-10 15:25:21 -0300

    Add argument parser

--- train_avispas.py
+++ train_avispas.py
@@ -1,10 +1,17 @@
 from ultralytics import YOLO
+import argparse
+
+parser = argparse.ArgumentParser(description="Entrenamiento de modelo YOLOv8 para el proyecto PICA-40-B-883")
+parser.add_argument('--dataset_path', default='dataset', help='Directorio del dataset en formato YOLOv8')
+parser.add_argument('--epochs', default=150, help='Cantidad de epochs de entrenamiento')
+parser.add_argument('--lr', default=0.015, help='Ritmo de aprendizaje')
+args = parser.parse_args()
 
 # Definimos el directorio del dataset con formato YOLOv8
-dataset = 'dataset'
+dataset = args.dataset_path
 
 # Cargamos modelo base
 model = YOLO('yolov8n.pt')
 
 # Entrenamos el modelo con el dataset
-results = model.train(data=f'{dataset}/data.yaml', epochs=150, imgsz=640, batch=-1, lr0=0.015)
+results = model.train(data=f'{dataset}/data.yaml', epochs=args.epochs, imgsz=640, batch=-1, lr0=args.lr)
