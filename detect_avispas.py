7d28c664e00c83032ec5f68b4f2d50cad6ab40af
Marian Basti <31198560+marianbasti@users.noreply.github.com>
2023-10-11 10:59:57 -0300

    Update detect_avispas.py
    Agregados columnas de detección de casta y confianza de prediccion (class, conf)
    Agregada columna del día (date)
    Reordenado orden de columnas, ahora filename está primero

--- detect_avispas.py
+++ detect_avispas.py
@@ -8,18 +8,14 @@ from statistics import mean
 
 from ultralytics import YOLO
 
-
-# import supervision as sv
-# from supervision.detection.line_counter import LineZone, LineZoneAnnotator
-
-parser = argparse.ArgumentParser(description="YOLOv8 Tracker")
+parser = argparse.ArgumentParser(description="YOLOv8 Tracker para el proyecto PICA-40-B-883")
 
 parser.add_argument('--model_path', default='best.pt', help='Modelo YOLOv8 ')
-parser.add_argument('--input_path', default="E:/2023_mp4/mp4/", help='Directorio de los videos (video/ + sensordata)')
+parser.add_argument('--input_path', default="./videos/", help='Directorio de los videos (video/ + sensordata)')
 parser.add_argument('--save_vid', action='store_true', help='Guardar video procesado')
-parser.add_argument('--txt_path', default='tracking', help='Nombre de salida para archivo CSV')
-parser.add_argument('--iou', default=0.5, help='IOU')
-parser.add_argument('--conf', default=0.5, help='confidence')
+parser.add_argument('--csv_output', default='tracking', help='Nombre de salida para archivo CSV')
+parser.add_argument('--iou', type=float, default=0.5, help='IOU')
+parser.add_argument('--conf', type=float, default=0.5, help='confidence')
 
 args = parser.parse_args()
 
@@ -30,9 +26,12 @@ input_path = args.input_path
 # Si guardamos video
 save_vid = args.save_vid
 # Output
-txt_path = args.txt_path
+csv_output = args.csv_output
+# Valor del threshold
+threshold_value = 90
 
-# line_annotator = LineZoneAnnotator()
+classes_names = ['worker','gyne','drone']
+
 
 def load_sensor_data(sensor_file):
     sensor_data = {}
@@ -48,16 +47,22 @@ def load_sensor_data(sensor_file):
 def interpolate(a, b, alpha):
     return a + alpha * (b - a)
 
+def parse_time(t):
+    try:
+        return datetime.strptime(t, '%H:%M:%S.%f')
+    except ValueError:
+        return datetime.strptime(t, '%H:%M:%S')
+
 def find_closest_data(time_str, sensor_data):
     times = sorted(sensor_data.keys())
     pos = bisect_left(times, time_str)
 
     if pos == 0 or pos == len(times):
         return sensor_data[times[0]] if pos == 0 else sensor_data[times[-1]]
-
-    before_time = datetime.strptime(times[pos - 1], '%H:%M:%S')
-    after_time = datetime.strptime(times[pos], '%H:%M:%S')
-    current_time = datetime.strptime(time_str, '%H:%M:%S')
+    
+    before_time = parse_time(times[pos - 1])
+    after_time = parse_time(times[pos])
+    current_time = parse_time(time_str)
     
     alpha = (current_time - before_time).total_seconds() / (after_time - before_time).total_seconds()
 
@@ -86,13 +91,10 @@ def refine_single_bbox(im, bbox_xyxy):
     except:
         print(f"Error with cropping image :\n{bbox_np}")
         
-    _, thresh = cv2.threshold(roi_gray, 85, 255, cv2.THRESH_BINARY_INV)
+    _, thresh = cv2.threshold(roi_gray, threshold_value, 255, cv2.THRESH_BINARY_INV)
     
     # Find contours
     contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    largest_contour = max(contours, key=cv2.contourArea)
-    translated_contour = largest_contour + [x1, y1]
-    cv2.polylines(annotated_frame, translated_contour, isClosed=True, color=(0, 255, 0), thickness=2)
 
     if len(contours) == 0:
         # box - 4 corners of the rectangle
@@ -103,6 +105,10 @@ def refine_single_bbox(im, bbox_xyxy):
         angle = 0
         min_rect = (center, size, angle)
         return box, min_rect
+    
+    largest_contour = max(contours, key=cv2.contourArea)
+    translated_contour = largest_contour + [x1, y1]
+    cv2.polylines(annotated_frame, translated_contour, isClosed=True, color=(0, 255, 0), thickness=3)
  
     # Find the smallest enclosing rotated rectangle for closed contours
     min_rect = cv2.minAreaRect(np.vstack(largest_contour))
@@ -130,9 +136,9 @@ def refine_single_bbox(im, bbox_xyxy):
     return box, min_rect
 
 # Inicializamos el archivo de salida
-if not os.path.exists(txt_path + '.csv'):
-        with open(txt_path + '.csv', 'a') as f:
-            f.write('id,timestamp,temp,humidity,largo,ancho,movement,time,filename\n')
+if not os.path.exists(csv_output + '.csv'):
+        with open(csv_output + '.csv', 'a') as f:
+            f.write('filename,id,date,timestamp,temp,humidity,length,width,movement,time,class,conf\n')
 
 # Por cada capeta que representa un día de filmaciones
 for source in os.listdir(input_path):
@@ -145,10 +151,10 @@ for source in os.listdir(input_path):
     sensor_data = load_sensor_data(sensor_file)
 
     # Por cada video en la carpeta "/video"
-    for video_path in os.listdir(input_path+'/'+source+'/video'):
+    for video_path in [f for f in os.listdir(input_path+'/'+source+'/video') if f.endswith('.mp4')]:
 
         # Inicializamos el historial de trackeos
-        track_history = defaultdict(lambda: {'points': [], 'timestamp': [], 'width': [], 'height': []})
+        track_history = defaultdict(lambda: {'points': [], 'timestamp': [], 'width': [], 'height': [], 'class': [], 'conf':[]})
 
         # Abrimos el video
         cap = cv2.VideoCapture(os.path.join(input_path,source)+'/video/'+video_path)
@@ -156,19 +162,17 @@ for source in os.listdir(input_path):
         # Obtenemos datos del video original
         frame_rate = int(cap.get(cv2.CAP_PROP_FPS))
         fps = cap.get(cv2.CAP_PROP_FPS)
-        w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-        h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        img_w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        img_h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
         if save_vid:  # Si queremos guardar el video para visualizar el trackeo
-            vid_writer = cv2.VideoWriter(f"{video_path}.avi", cv2.VideoWriter_fourcc(*'mp4v'), fps, (w, h))
+            vid_writer = cv2.VideoWriter(f"{video_path.split('.mp4')[0]}.avi", cv2.VideoWriter_fourcc(*'mp4v'), fps, (img_w, img_h))
 
         # Limite para considerar salida/entrada
-        significant_move = w/4
+        significant_move = img_w/4
 
-        # Contador de Supervision
-        # LINE_START = sv.Point(w/2, 0)
-        # LINE_END = sv.Point(w/2, h)
-        # line_counter = sv.LineZone(start=LINE_START, end=LINE_END)
+        # Limite para ignorar altos y anchos (en los bordes confunde)
+        border_threshold = img_w/5
 
         # Extraemos la hora
         time_str = video_path.split(' ')[-1].replace('_', ':').split('.')[0]
@@ -182,14 +186,10 @@ for source in os.listdir(input_path):
             if success:
                 # Corremos YOLOv8
                 results = model.track(frame, persist=True, conf=args.conf, iou=args.iou, tracker='avispa_bytetrack.yaml')
-                annotated_frame = results[0].plot()
-                
-                # Supervision
-                # detections = sv.Detections.from_ultralytics(results[0])
-                # line_counter.trigger(detections=detections)
 
                 # Obtenemos cajas e IDs detectadas
                 boxes = results[0].boxes.xywh.cpu()
+                annotated_frame = results[0].plot()
 
                 # Si hay detección
                 if results[0].boxes:
@@ -197,17 +197,21 @@ for source in os.listdir(input_path):
                         # Obtenemos datos temporales de la detección
                         current_frame = int(cap.get(cv2.CAP_PROP_POS_FRAMES))
                         elapsed_time = timedelta(seconds=current_frame / frame_rate)
-                        timestamp = (initial_time + elapsed_time).strftime('%H:%M:%S')
+                        timestamp = (initial_time + elapsed_time).strftime('%H:%M:%S.%f')
                         track_ids = results[0].boxes.id.int().cpu().tolist()
 
                         # Por cada detección
-                        for box, track_id in zip(boxes, track_ids):
+                        for idx, (box, track_id) in enumerate(zip(boxes, track_ids)):
 
                             # Guardamos los puntos, timestamp y tamaños de la detección
                             x, y, w, h = box
                             track = track_history[track_id]
                             track['points'].append((float(x), float(y)))
                             track['timestamp'].append(timestamp)
+
+                            # Guardamos la clase de la detección
+                            track['conf'].append(float(results[0].boxes.conf[idx].float().cpu().tolist()))
+                            track['class'].append(results[0].boxes.cls[idx].int().cpu().tolist())
 
                             # Refinamos el tamaño para que sea el mas compacto posible
                             refined_xyxy,min_rect = refine_single_bbox(frame,box)
@@ -217,11 +221,13 @@ for source in os.listdir(input_path):
 
                             # Por último dibujamos las líneas del trackeo
                             points = np.hstack(track['points']).astype(np.int32).reshape((-1, 1, 2))
-                            cv2.polylines(annotated_frame, [points], isClosed=False, color=(255, 20, 0), thickness=4)
+                            if x>border_threshold and x<img_w-border_threshold:
+                                cv2.polylines(annotated_frame, [points], isClosed=False, color=(20, 255, 0), thickness=4)
+                            else:
+                                cv2.polylines(annotated_frame, [points], isClosed=False, color=(0, 20, 250), thickness=4)
 
                 # Mostramos la imagen
                 cv2.imshow("YOLOv8 Tracking", annotated_frame)
-                # cv2.imshow("YOLOv8 Tracking", line_annotator.annotate(annotated_frame, line_counter))
                 if save_vid:  # Si guardamos el video procesado
                     vid_writer.write(annotated_frame)
                 # Pasar al video siguiente si apretamos 'n'
@@ -247,26 +253,39 @@ for source in os.listdir(input_path):
                 elif final_x < initial_x:
                     print(f"Object {id} moved in.")
                     str_movement='in'
+            else:
+                str_movement='undetermined'
 
-                # Formateamos data
-                timestamp = track_history[id]['timestamp'][0]
-                data = find_closest_data(timestamp,sensor_data)
-                temp = data.get('temp', 'N/A')
-                humidity = data.get('humidity', 'N/A')
+            # Formateamos data
+            timestamp = track_history[id]['timestamp'][0]
+            data = find_closest_data(timestamp,sensor_data)
+            temp = data.get('temp', 'N/A')
+            humidity = data.get('humidity', 'N/A')
 
-                # Promediamos altos y anchos
-                avg_w = mean(track_history[id]['width'])
-                avg_h = mean(track_history[id]['height'])
+            filtered_widths = [w for w, (x, _) in zip(track_history[id]['width'], track_history[id]['points']) if x > border_threshold and x < (img_w - border_threshold)]
+            filtered_heights = [h for h, (x, _) in zip(track_history[id]['height'], track_history[id]['points']) if x > border_threshold and x < (img_w - border_threshold)]
 
-                # Calculamos cuanto tiempo tardó en entrar/salir
-                time_taken = datetime.strptime(track_history[id]['timestamp'][-1],'%H:%M:%S') - datetime.strptime(track_history[id]['timestamp'][0], '%H:%M:%S')
+            if len(filtered_widths)>0:
+                avg_w = round(mean(filtered_widths),2)
+                avg_h = round(mean(filtered_heights),2)
+            else:
+                avg_w = "N/A"
+                avg_h = "N/A"
 
-                # Armamos el log   
-                log_data=f"""{id},{timestamp},{round(temp,2)},{round(humidity,2)},{round(avg_w,2)},{round(avg_h,2)},{str_movement},{time_taken},{video_path}\n"""
+            # Calculamos cuanto tiempo tardó en entrar/salir
+            time_taken = datetime.strptime(track_history[id]['timestamp'][-1],'%H:%M:%S.%f') - datetime.strptime(track_history[id]['timestamp'][0], '%H:%M:%S.%f')
 
-                # Agregamos la data del video en el csv
-                with open(f"{txt_path}.csv", "a") as f:
-                    f.write(log_data)
+            # Clasificación
+            max_index = track_history[id]['conf'].index(max(track_history[id]['conf']))
+            conf = round(track_history[id]['conf'][max_index],2)
+            classd = track_history[id]['class'][max_index]
+
+            # Armamos el log   
+            log_data=f"""{video_path},{id},{video_path.split(' ')[0]},{str(timestamp)[:-4]},{round(temp,2)},{round(humidity,2)},{str(avg_w).replace('.',',')},{str(avg_h).replace('.',',')},{str_movement},{str(time_taken)[2:-4]},{classes_names[classd]},{conf}\n"""
+
+            # Agregamos la data del video en el csv
+            with open(f"{csv_output}.csv", "a") as f:
+                f.write(log_data)
 
         # Terminar si apretamos 'q'
         if cv2.waitKey(1) & 0xFF == ord("q"):
