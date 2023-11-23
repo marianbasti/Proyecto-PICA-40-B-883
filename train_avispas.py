8e66ad942c04aeae3ab24811f416171e6d9398b6
marianbasti <marianobasti@gmail.com>
2023-11-23 19:36:35 -0300

    Nuevo entrenamiento, mejor deteccion de castas

--- train_avispas.py
+++ train_avispas.py
@@ -2,16 +2,18 @@ from ultralytics import YOLO
 import argparse
 
 parser = argparse.ArgumentParser(description="Entrenamiento de modelo YOLOv8 para el proyecto PICA-40-B-883")
+parser.add_argument('--model', default='yolov8n.pt', help='Modelo base para entrenar')
 parser.add_argument('--dataset_path', default='dataset', help='Directorio del dataset en formato YOLOv8')
 parser.add_argument('--epochs', default=150, help='Cantidad de epochs de entrenamiento')
 parser.add_argument('--lr', default=0.015, help='Ritmo de aprendizaje')
 args = parser.parse_args()
 
-# Definimos el directorio del dataset con formato YOLOv8
-dataset = args.dataset_path
+if __name__ == '__main__':
+    # Definimos el directorio del dataset con formato YOLOv8
+    dataset = args.dataset_path
 
-# Cargamos modelo base
-model = YOLO('yolov8n.pt')
+    # Cargamos modelo base
+    model = YOLO('yolov8n.pt')
 
-# Entrenamos el modelo con el dataset
-results = model.train(data=f'{dataset}/data.yaml', epochs=args.epochs, imgsz=640, batch=-1, lr0=args.lr)
+    # Entrenamos el modelo con el dataset
+    results = model.train(data=f'{dataset}/data.yaml', epochs=args.epochs, imgsz=640, batch=-1, lr0=args.lr)
