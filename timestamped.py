8e1ca17d5ac266ed1042cfe208f3ddcdeb786ff4
Marian Basti <31198560+marianbasti@users.noreply.github.com>
2024-03-05 21:29:16 -0300

    Update timestamped.py
    - Añade argparser para ajustar resolución, framerate, shutter_speed y control de horario de filmación
    - Función ensure_dir para optimizar creación de directorios
    - Menos conversiones de datetime a string
    - Consolida nombre de archivo y directorios
    - Legibilidad y mantenibilidad del código

--- timestamped.py
+++ timestamped.py
@@ -1,38 +1,49 @@
 import picamera
 import datetime as dt
 import os
+import argparse
+
+# Parsea los argumentos de la línea de comandos
+parser = argparse.ArgumentParser(description='Configura la cámara para grabar.')
+parser.add_argument('--resolution', type=str, default='1600x1080', help='Resolución de la cámara como WxH')
+parser.add_argument('--framerate', type=int, default=10, help='Framerate de la cámara')
+parser.add_argument('--shutter_speed', type=int, default=800, help='Velocidad del obturador de la cámara')
+parser.add_argument('--start_hour', type=int, default=7, help='Hora de inicio de la filmación')
+parser.add_argument('--end_hour', type=int, default=19, help='Hora de fin de la filmación')
+args = parser.parse_args()
+
+# Extrae la resolución
+resolution = tuple(map(int, args.resolution.split('x')))
+
+def ensure_dir(directory):
+    os.makedirs(directory, exist_ok=True)
 
 with picamera.PiCamera() as camera:
-    camera.resolution = (1600, 1080)
-    camera.framerate = 10
-    #camera.start_preview()
+    camera.resolution = resolution
+    camera.framerate = args.framerate
     camera.annotate_background = picamera.Color('black')
-    camera.annotate_text = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    
+    camera.shutter_speed = args.shutter_speed
+
     while True:
-        now = str(dt.datetime.now())
-        timestamp=now.split()
-        milis=timestamp[1].split(".")
-        date=timestamp[0].split("-")
-        time=milis[0].split(":")
-        camera.shutter_speed=800
-        if int(time[0])>=7 and int(time[0])<=19:
-            print (now)
-            try:
-                os.mkdir("/home/pi/output/"+timestamp[0])
-            except OSError as error:
-                pass
-            try:
-                os.mkdir("/home/pi/output/"+timestamp[0]+"/video")
-            except OSError as error:
-                pass
-            #camera.start_recording("/home/pi/output/"+timestamp[0]+"/video/"+timestamp[0]+" "+milis[0]+'.h264')
-            camera.start_recording("/home/pi/output/"+timestamp[0]+"/video/"+date[0]+"_"+date[1]+"_"+date[2]+"_"+"_"+time[0]+"_"+time[1]+"_"+time[2]+'.h264')
+        now = dt.datetime.now()
+        camera.annotate_text = now.strftime('%Y-%m-%d %H:%M:%S')
+
+        if args.start_hour <= now.hour <= args.end_hour:
+            print(now)
+            date_str = now.strftime('%Y-%m-%d')
+            time_str = now.strftime('%H_%M_%S')
+            base_path = f"/home/pi/output/{date_str}"
+            video_path = f"{base_path}/video"
+            
+            ensure_dir(video_path)
+
+            video_filename = f"{video_path}/{date_str}_{time_str}.h264"
+            camera.start_recording(video_filename)
+
             start = dt.datetime.now()
             while (dt.datetime.now() - start).seconds < 900:
                 camera.annotate_text = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
                 camera.wait_recording(0.2)
+
             camera.stop_recording()
-            print ("video finished")
-        #if int(time[0])==1 and (time[1])==10:
-            #exit()
+            print("Video terminado")
